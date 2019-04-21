ff1d4e01be26ec88af87d618920cffeb5eca4c2b
Krikod <kristen.kod@gmail.com>
2019-04-21 13:46:12 +0200

    inserting objects

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -4,10 +4,36 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use AppBundle\Entity\Genus;
 
 class GenusController extends Controller
 {
+    /**
+     * @Route("/genus/new")
+     */
+    public function newAction()
+    {
+        $genus = new Genus();
+        $genus->setName('Octopus'.rand(1, 100));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($genus);
+        $em->flush();
+
+        return new Response('<html><body>Genus Created</body></html>');
+    }
+
+    /**
+     * @Route("/genus/(genusName)")
+     * @param $genusName
+     */
+    public function showAction($genusName)
+    {
+
+    }
+
     /**
      * @Route("/", name="homepage")
      */
